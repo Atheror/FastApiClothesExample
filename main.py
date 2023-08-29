808dd9ce7c2932234d4c9d4dcc512688a64ade0e
Juan S. López <dir.desarrollo@whitecloud.com.co>
2023-08-29 17:36:18 -0500

    format document

--- main.py
+++ main.py
@@ -20,10 +20,12 @@ database = databases.Database(DATABASE_URL)
 
 metadata = sqlalchemy.MetaData()
 
+
 class UserRole(enum.Enum):
     super_admin = "super_admin"
     admin = "admin"
     user = "user"
+
 
 users = sqlalchemy.Table(
     "users",
@@ -33,7 +35,8 @@ users = sqlalchemy.Table(
     sqlalchemy.Column("password", sqlalchemy.String(255)),
     sqlalchemy.Column("full_name", sqlalchemy.String(200)),
     sqlalchemy.Column("phone", sqlalchemy.String(13)),
-    sqlalchemy.Column("created_at", sqlalchemy.DateTime, nullable=False, server_default=sqlalchemy.func.now()),
+    sqlalchemy.Column("created_at", sqlalchemy.DateTime,
+                      nullable=False, server_default=sqlalchemy.func.now()),
     sqlalchemy.Column(
         "last_modified_at",
         sqlalchemy.DateTime,
@@ -41,9 +44,10 @@ users = sqlalchemy.Table(
         server_default=sqlalchemy.func.now(),
         onupdate=sqlalchemy.func.now(),
     ),
-    sqlalchemy.Column("role", sqlalchemy.Enum(UserRole), nullable=False, default=UserRole.user, server_default=UserRole.user.value),
+    sqlalchemy.Column("role", sqlalchemy.Enum(UserRole), nullable=False,
+                      default=UserRole.user, server_default=UserRole.user.value),
 )
- 
+
 
 class ColorEnum(enum.Enum):
     pink = "pink"
@@ -60,6 +64,7 @@ class SizeEnum(enum.Enum):
     xl = "xl"
     xxl = "xxl"
 
+
 clothes = sqlalchemy.Table(
     "clothes",
     metadata,
@@ -68,7 +73,8 @@ clothes = sqlalchemy.Table(
     sqlalchemy.Column("color", sqlalchemy.Enum(ColorEnum), nullable=False),
     sqlalchemy.Column("size", sqlalchemy.Enum(SizeEnum), nullable=False),
     sqlalchemy.Column("photo_url", sqlalchemy.String(255)),
-    sqlalchemy.Column("created_at", sqlalchemy.DateTime, nullable=False, server_default=sqlalchemy.func.now()),
+    sqlalchemy.Column("created_at", sqlalchemy.DateTime,
+                      nullable=False, server_default=sqlalchemy.func.now()),
     sqlalchemy.Column(
         "last_modified_at",
         sqlalchemy.DateTime,
@@ -78,11 +84,12 @@ clothes = sqlalchemy.Table(
     ),
 )
 
+
 class EmailField(str):
     @classmethod
     def __get_validators__(cls):
         yield cls.validate
-    
+
     @classmethod
     def validate(cls, v) -> str:
         try:
@@ -91,42 +98,47 @@ class EmailField(str):
         except EmailNotValidError:
             raise ValueError("Email is not valid")
 
+
 class BaseUser(BaseModel):
     email: EmailField
     full_name: str
-    
+
     @validator('email')
-    def validate_email(cls,v):
+    def validate_email(cls, v):
         try:
             validate_e(v)
             return v
         except EmailNotValidError:
-            raise ValueError("Email is not valid") 
-    
+            raise ValueError("Email is not valid")
+
     @validator('full_name')
-    def validate_full_name(cls,v):
+    def validate_full_name(cls, v):
         try:
-           first_name, last_name = v.split()
-           return v
+            first_name, last_name = v.split()
+            return v
         except Exception:
             raise ValueError("You should enter your first name and last name")
+
 
 class UserSignIn(BaseUser):
     password: str
     role: UserRole = UserRole.user
 
+
 app = FastAPI()
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 class CustomHTTPBearer(HTTPBearer):
     async def __call__(
         self, request: Request
     ) -> Optional[HTTPAuthorizationCredentials]:
         res = await super().__call__(request)
-    
+
         try:
-            payload = jwt.decode(res.credentials, config("JWT_SECRET"), algorithms=["HS256"])
+            payload = jwt.decode(res.credentials, config(
+                "JWT_SECRET"), algorithms=["HS256"])
             user = await database.fetch_one(users.select().where(users.c.id == payload["sub"]))
             request.state.user = user
             return payload
@@ -135,12 +147,15 @@ class CustomHTTPBearer(HTTPBearer):
         except jwt.InvalidTokenError:
             raise HTTPException(401, "Token is invalid")
 
+
 oauth2_scheme = CustomHTTPBearer()
+
 
 def is_admin(request: Request):
     user = request.state.user
     if not user or user["role"] not in (UserRole.admin, UserRole.super_admin):
         raise HTTPException(403, "You are not allowed to perform this action")
+
 
 def create_access_token(user):
     try:
@@ -152,18 +167,22 @@ def create_access_token(user):
     except Exception as ex:
         raise ex
 
+
 @app.on_event("startup")
 async def startup():
     await database.connect()
+
 
 @app.on_event("shutdown")
 async def shutdown():
     await database.disconnect()
 
+
 @app.get("/clothes/", dependencies=[Depends(oauth2_scheme)])
 async def get_all_clothes(request: Request):
     user = request.state.user
     return await database.fetch_all(clothes.select())
+
 
 class ClothesBase(BaseModel):
     name: str
@@ -171,13 +190,16 @@ class ClothesBase(BaseModel):
     size: SizeEnum
     photo_url: str
 
+
 class ClothesIn(ClothesBase):
     pass
+
 
 class ClothesOut(ClothesBase):
     id: int
     created_at: datetime
     last_modified_at: datetime
+
 
 @app.post("/clothes/",
           response_model=ClothesOut,
@@ -186,8 +208,7 @@ class ClothesOut(ClothesBase):
           )
 async def create_clothes(clothes_data: ClothesIn):
     id_ = await database.execute(clothes.insert().values(**clothes_data.dict()))
-    return await database.fetch_one(clothes.select().where( clothes.c.id == id_))
-
+    return await database.fetch_one(clothes.select().where(clothes.c.id == id_))
 
 
 @app.post("/register/", status_code=201)
